619c857cff8430acca0c61d4ff6bb7fc434d4573
IharTsykala <sudja09@bk.ru>
2019-09-27 11:37:28 +0300

    Updates

--- precache-manifest.d5aad3d0a13e64861255b23dad99c984.js
+++ precache-manifest.d5aad3d0a13e64861255b23dad99c984.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "11b66a2f92b35518a6d851de9085bf4a",
+    "revision": "e9856fd9439d3191fc8b193791258447",
     "url": "/InteticsLab-2019/index.html"
   },
   {
-    "revision": "7a75e09fb28c51aebb2f",
-    "url": "/InteticsLab-2019/static/css/main.621021c0.chunk.css"
+    "revision": "4638cb37969567955531",
+    "url": "/InteticsLab-2019/static/css/main.f80015a3.chunk.css"
   },
   {
     "revision": "c6976c5fa0c44c403023",
     "url": "/InteticsLab-2019/static/js/2.3c0b5141.chunk.js"
   },
   {
-    "revision": "7a75e09fb28c51aebb2f",
-    "url": "/InteticsLab-2019/static/js/main.4e7eadcf.chunk.js"
+    "revision": "4638cb37969567955531",
+    "url": "/InteticsLab-2019/static/js/main.13e4bf66.chunk.js"
   },
   {
     "revision": "9d10ebab77689330b905",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/InteticsLab-2019/precache-manifest.b0c9505706024dedaabe1ec88c9e9dfe.js"
+  "/InteticsLab-2019/precache-manifest.d5aad3d0a13e64861255b23dad99c984.js"
 );
 
 self.addEventListener('message', (event) => {
